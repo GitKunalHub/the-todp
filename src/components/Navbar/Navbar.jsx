ead526c2d86644818eb44d20dc466c4dc37dada9
GitKunalHub <kunalgolani01@gmail.com>
2024-04-11 06:20:46 +0530

    Link onClick event handling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,22 +8,26 @@ import images from "../../constants/images";
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
+  const handleLinkClick = () => {
+    setToggleMenu(false);
+  };
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
         <img src={images.gericht} alt="app logo" />
       </div>
       <ul className="app__navbar-links">
-        <li className="p__opensans">
+        <li className="p__opensans" onClick={handleLinkClick}>
           <a href="#home">Home</a>
         </li>
-        <li className="p__opensans">
+        <li className="p__opensans" onClick={handleLinkClick}>
           <a href="#about">About</a>
         </li>
-        <li className="p__opensans">
+        <li className="p__opensans" onClick={handleLinkClick}>
           <a href="#menu">Menu</a>
         </li>
-        <li className="p__opensans">
+        <li className="p__opensans" onClick={handleLinkClick}>
           <a href="#contact">Contact</a>
         </li>
       </ul>
@@ -42,16 +46,16 @@ const Navbar = () => {
               onClick={() => setToggleMenu(false)}
             />
             <ul className="app__navbar-smallscreen_links">
-              <li className="p__opensans">
+              <li className="p__opensans" onClick={handleLinkClick}>
                 <a href="#home">Home</a>
               </li>
-              <li className="p__opensans">
+              <li className="p__opensans" onClick={handleLinkClick}>
                 <a href="#about">About</a>
               </li>
-              <li className="p__opensans">
+              <li className="p__opensans" onClick={handleLinkClick}>
                 <a href="#menu">Menu</a>
               </li>
-              <li className="p__opensans">
+              <li className="p__opensans" onClick={handleLinkClick}>
                 <a href="#contact">Contact</a>
               </li>
             </ul>
