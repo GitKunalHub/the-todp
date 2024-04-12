837e4372ad2622b2835354330a73aa6dc8b094fc
GitKunalHub <kunalgolani01@gmail.com>
2024-04-12 15:17:00 +0530

    Template-2

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,22 +8,22 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="about"
   >
-    <div className="app__aboutus-overlay flex__center">
+    {/* <div className="app__aboutus-overlay flex__center">
       <img src={images.G} />
-    </div>
+    </div> */}
 
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">
+        <p className="p__opensanss">
           We started as a catering service, specializing in the irresistible
           combination of crispy pakwan and soul-warming dal. Our guests couldn't
           get enough, and by popular demand, we opened our doors in 2019! We
           still hold the legacy of our beginnings close, and you can always find
           our legendary Dal Pakwan on the menu.
         </p>
-        <button
+        {/* <button
           type="button"
           className="custom__button"
           onClick={() => {
@@ -31,7 +31,7 @@ const AboutUs = () => (
           }}
         >
           Know More
-        </button>
+        </button> */}
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
@@ -41,7 +41,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">
+        <p className="p__opensanss">
           Back in 2007, a seed was sown – a seed of sharing our love for the
           perfect Dal Pakwan. What began as a catering service, overflowing with
           rave reviews, blossomed into the restaurant you know and love today.
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -14,16 +14,16 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">
+        <p className="p__opensanss">
           Shop No. 35 Rajmahal Complex, Behind Surya Residency, nr. Dhiraj Sons,
           Vesu, Surat, Gujarat 395007
         </p>
         <br />
-        <p className="p__opensans">+91 8200544305</p>
+        <p className="p__opensanss">+91 8200544305</p>
       </div>
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
-        <p className="p__opensans">Making Memories, One Pakwan at a Time.</p>
+        <p className="p__opensanss">Making Memories, One Pakwan at a Time.</p>
         <img
           src={images.spoon}
           alt="spoon"
@@ -51,10 +51,10 @@ const Footer = () => (
       </div>
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">07:30 Am - 09:00 Pm</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">09:30 Am - 10:30 Pm</p>
+        <p className="p__opensanss">Monday-Friday:</p>
+        <p className="p__opensanss">07:30 Am - 09:00 Pm</p>
+        <p className="p__opensanss">Saturday-Sunday:</p>
+        <p className="p__opensanss">09:30 Am - 10:30 Pm</p>
       </div>
     </div>
     <div className="footer__copyright">
