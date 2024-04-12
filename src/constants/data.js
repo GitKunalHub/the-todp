23562275264274f6e7e42581c5e3877bc5b5e942
GitKunalHub <kunalgolani01@gmail.com>
2024-04-12 15:23:59 +0530

    Template-2

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,97 +1,83 @@
 import images from "./images";
 
-const wines = [
-  {
-    title: "Dal Pakwan",
-    price: "₹70",
-    tags: "IN | Meal",
-  },
-  {
-    title: "Sindhi Kadhi Chawal",
-    price: "₹150",
-    tags: "IN | Meal",
-  },
-  {
-    title: "Butter Pav Bhaji",
-    price: "₹120",
-    tags: "IN | Meal",
-  },
-  {
-    title: "Chole Tikki",
-    price: "₹60",
-    tags: "IN | Plate",
-  },
-  {
-    title: "Veg Sandwich",
-    price: "₹50",
-    tags: "IN | Plate",
-  },
-  {
-    title: "Veg Burger",
-    price: "₹60",
-    tags: "IN | Plate",
-  },
-  {
-    title: "Dahi Papdi Chaat",
-    price: "₹50",
-    tags: "IN | Plate",
-  },
-];
+const data = {
+  the_todp_specials: [
+    { title: "Dal Pakwan", price: "₹70", tags: "IN | Meal" },
+    { title: "Chole Pakwan", price: "₹70", tags: "IN | Meal" },
+    { title: "Cheese Dal Pakwan", price: "₹110", tags: "IN | Meal" },
+    { title: "Cheese Chole Pakwan", price: "₹110", tags: "IN | Meal" },
+    { title: "Dal Pakwan Bhel", price: "₹70", tags: "IN | Meal" },
+    { title: "Cheese Masala Pakwan", price: "₹80", tags: "IN | Meal" },
+  ],
+  chaat_specials: [
+    { title: "Chole Tikki", price: "₹60", tags: "IN | Plate" },
+    { title: "Dahi Chole Tikki", price: "₹70", tags: "IN | Plate" },
+    { title: "Cheese Chole Tikki", price: "₹90", tags: "IN | Plate" },
+    { title: "Aloo Tikki with Chutney", price: "₹30", tags: "IN | Plate" },
+    { title: "Chole Tikki Pav", price: "₹75", tags: "IN | Plate" },
+    { title: "Dahi Bhalla (Dahi Vada)", price: "₹50", tags: "IN | Plate" },
+    { title: "Dahi Papdi Chaat", price: "₹50", tags: "IN | Plate" },
+    { title: "Dahi Tikki", price: "₹40", tags: "IN | Plate" },
+    { title: "Bhel Puri", price: "₹50", tags: "IN | Plate" },
+  ],
+  sandwich_specials: [
+    { title: "Veg. Sandwich", price: "₹50", tags: "IN | Plate" },
+    { title: "Veg. Cheese Sandwich", price: "₹80", tags: "IN | Plate" },
+    { title: "Veg. Grilled Sandwich", price: "₹90", tags: "IN | Plate" },
+    {
+      title: "Veg. Cheese Grilled Sandwich",
+      price: "₹120",
+      tags: "IN | Plate",
+    },
+    { title: "Masala Grilled Sandwich", price: "₹100", tags: "IN | Plate" },
+    {
+      title: "Aloo Masala Cheese Grilled Sandwich",
+      price: "₹130",
+      tags: "IN | Plate",
+    },
+    { title: "Bread Butter Toast", price: "₹40", tags: "IN | Plate" },
+    { title: "Bread Butter", price: "₹30", tags: "IN | Plate" },
+    { title: "Bread Butter Cheese", price: "₹60", tags: "IN | Plate" },
+  ],
+  sindhi_meal_specials: [
+    { title: "Sindhi Kadhi Chawal", price: "₹150", tags: "IN | Meal" },
+  ],
+  pav_bhaji_specials: [
+    { title: "Butter Pav Bhaji", price: "₹120", tags: "IN | Meal" },
+    { title: "Cheese Pav Bhaji", price: "₹160", tags: "IN | Meal" },
+    { title: "Butter Masala Pav", price: "₹30", tags: "IN | Meal" },
+    { title: "Cheese Masala Pav", price: "₹60", tags: "IN | Meal" },
+  ],
+  burger_specials: [
+    { title: "Veg. Burger", price: "₹60", tags: "IN | Meal" },
+    { title: "Veg. Cheese Burger", price: "₹80", tags: "IN | Meal" },
+    { title: "Double Patty Cheese Burger", price: "₹100", tags: "IN | Meal" },
+    { title: "Double Cheese Veg. Burger", price: "₹100", tags: "IN | Meal" },
+  ],
+  dessert_specials: [
+    { title: "Gulab Jamun", price: "₹30", tags: "IN | Dessert" },
+    { title: "Sooji Halwa (Pure Ghee)", price: "₹50", tags: "IN | Dessert" },
+  ],
+  beverage_specials: [
+    { title: "Special Masala Tea", price: "₹20", tags: "IN | Beverages" },
+    { title: "Special Coffee", price: "₹20", tags: "IN | Beverages" },
+    { title: "Special Masala Chaas", price: "₹20", tags: "IN | Beverages" },
+    { title: "Soft Drinks", price: "MRP", tags: "IN | Beverages" },
+  ],
+  extras: [
+    { title: "Extra Dal", price: "₹35", tags: "IN | Extras" },
+    { title: "Extra Chole", price: "₹40", tags: "IN | Extras" },
+    {
+      title: "Extra Pakwan (Without Chutney)",
+      price: "₹35",
+      tags: "IN | Extras",
+    },
+    { title: "Extra Pakwan (With Chutney)", price: "₹40", tags: "IN | Extras" },
+    { title: "Extra Pav (4 pcs.)", price: "₹30", tags: "IN | Extras" },
+    { title: "Extra Pav (2 pcs.)", price: "₹20", tags: "IN | Extras" },
+    { title: "Extra Bhaji", price: "₹80", tags: "IN | Extras" },
+    { title: "Extra Sindhi Kadhi", price: "₹80", tags: "IN | Extras" },
+  ],
+};
 
-const cocktails = [
-  {
-    title: "Bread Butter Toast",
-    price: "₹40",
-    tags: "IN | Snacks",
-  },
-  {
-    title: "Dahi Bhalla (Dahi Vada)",
-    price: "₹50",
-    tags: "IN | Chaat",
-  },
-  {
-    title: "Sooji Halwa (Pure Ghee)",
-    price: "₹50",
-    tags: "IN | Dessert",
-  },
-  {
-    title: "Gulab Jamun",
-    price: "₹30",
-    tags: "IN | Dessert",
-  },
-  {
-    title: "Special Masala Tea/Coffee",
-    price: "₹20",
-    tags: "IN | Beverages",
-  },
-  {
-    title: "Cheese Masala Pav",
-    price: "₹60",
-    tags: "IN | Snacks",
-  },
-];
-
-const awards = [
-  {
-    imgUrl: images.award02,
-    title: "Bib Gourmond",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-  },
-  {
-    imgUrl: images.award01,
-    title: "Rising Star",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-  },
-  {
-    imgUrl: images.award05,
-    title: "AA Hospitality",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-  },
-  {
-    imgUrl: images.award03,
-    title: "Outstanding Chef",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-  },
-];
-
-export default { wines, cocktails, awards };
+export default data;
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { images, data } from "../../constants";
 
@@ -6,58 +6,69 @@ import { SubHeading, MenuItem } from "../../components";
 
 import "./SpecialMenu.css";
 
-const SpecialMenu = () => (
-  <div className="app__specialMenu flex__center section__padding" id="menu">
-    <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits your palatte" />
-      <h1 className="headtext__cormorant">Today's Special</h1>
-    </div>
+const SpecialMenu = () => {
+  const [activeTab, setActiveTab] = useState("the_todp_specials"); // Set initial active tab
 
-    <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine flex__center">
-        <p className="app__specialMenu-menu_heading">The TODP's Signature</p>
-        <div className="app__specialMenu_menu_items">
-          {data.wines.map((wine, index) => (
-            <MenuItem
-              key={wine.title}
-              price={wine.price}
-              title={wine.title}
-              tags={wine.tags}
-            />
+  const handleClick = (category) => {
+    setActiveTab(category);
+  };
+
+  const renderMenu = (category) => {
+    return data[category].map((item, index) => (
+      <MenuItem
+        key={item.title}
+        price={item.price}
+        title={item.title}
+        tags={item.tags}
+      />
+    ));
+  };
+
+  const categories = Object.keys(data); // Get all categories as an array
+
+  return (
+    <div className="app__specialMenu flex__center section__padding" id="menu">
+      <div className="app__specialMenu-title">
+        <SubHeading title="Menu that fits your palatte" />
+        <h1 className="headtext__cormorant">Today's Special</h1>
+      </div>
+
+      <div className="app__specialMenu-tabs">
+        <ul className="app__specialMenu-tabs_list">
+          {categories.map((category) => (
+            <li
+              key={category}
+              className={`app__specialMenu-tabs_item ${
+                activeTab === category ? "active" : ""
+              }`}
+              onClick={() => handleClick(category)}
+            >
+              {category.replace(/_/g, " ")}{" "}
+              {/* Replace underscores with spaces in category names */}
+            </li>
           ))}
+        </ul>
+      </div>
+
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_items">
+          {renderMenu(activeTab)}
         </div>
       </div>
 
-      <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="menu img" />
-      </div>
-
-      <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu-menu_heading">Breakfast and Beverages</p>
-        <div className="app__specialMenu_menu_items">
-          {data.cocktails.map((cocktail, index) => (
-            <MenuItem
-              key={cocktail.title}
-              price={cocktail.price}
-              title={cocktail.title}
-              tags={cocktail.tags}
-            />
-          ))}
-        </div>
+      <div style={{ marginTop: "15px" }}>
+        <button
+          type="button"
+          className="custom__button"
+          onClick={() => {
+            window.location.href = "https://i.ibb.co/TTcp3V0/download.png";
+          }}
+        >
+          View More
+        </button>
       </div>
     </div>
-    <div style={{ marginTop: "15px" }}>
-      <button
-        type="button"
-        className="custom__button"
-        onClick={() => {
-          window.location.href = "https://i.ibb.co/TTcp3V0/download.png";
-        }}
-      >
-        View More
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default SpecialMenu;
